abb234b02a050d3bb021be661a70eef810acea9b
EC2 Default User <gusdnddk111@gmail.com>
2016-11-25 18:03:10 +0000

    asd

--- crawller.py
+++ crawller.py
@@ -1,10 +1,24 @@
 #-*- coding: utf-8 -*-
 import requests
 import urllib
-
+import csv
 from bs4 import BeautifulSoup
 
+count=0
+
 bookURL_List=[]
+book_name = []
+book_price = []
+book_writer = []
+book_publisher = []
+book_category = []
+book_page = []
+book_size = []
+book_cover = []
+book_weight = []
+book_ISBN = []
+book_date = []
+book_sales_point = []
 
 def spider(page):
     url = 'http://www.aladin.co.kr/shop/common/wnew.aspx?ViewRowsCount=50&ViewType=Detail&BranchType=1&PublishDay=168&NewType=SpecialNew&page=' + str(page)
@@ -16,44 +30,40 @@ def spider(page):
         href = tag.get('href')
         bookURL_List.append(href)
 
-    print (len(bookURL_List))
-
 
 #1페이지부터 i-1페이지까지
 for i in range(1,2):
     spider(i)
 
-fp = open("book_data.txt", 'w')
-fp.write("제목\t작가\t출판사\t출판날짜\t가격\t페이지수\t크기\t표지\t무게\t분류\tISBN\tsales point\n")
-
 #책 하나하나 정보 크롤링하기
-for i in range(0,len(bookURL_List)):
+#for i in range(0,len(bookURL_List)):
+for i in range(0,1):
     book_url = bookURL_List.pop()
     source_code = requests.get(book_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'lxml')
     
-    book_name = "NA"
-    book_price = "NA"
-    book_writer = "NA"
-    book_publisher = "NA"
-    book_category = "NA"
-    book_page = "NA"
-    book_size = "NA"
-    book_cover = "NA"
-    book_weight = "NA"
-    book_ISBN = "NA"
-    book_date = "NA"
+    name = "NA"
+    price = "NA"
+    writer = "NA"
+    publisher = "NA"
+    category = "NA"
+    page = "NA"
+    size = "NA"
+    cover = "NA"
+    weight = "NA"
+    ISBN = "NA"
+    date = "NA"
     sales_point = "NA"
 
 
-    book_name = soup.find("a",{"class":"p_topt01"}).text
+    name = soup.find("a",{"class":"p_topt01"}).text
     #책이름
-    book_price = soup.find("span",{"class":"p_new_price_ph"}).text
+    price = soup.find("span",{"class":"p_new_price_ph"}).text
     #책가격
     sales_point = soup.find("ul",{"class":"r_topsidebox"}).find("div").find("strong").text
     #판매량
-    book_category = soup.findAll("a",{"class":"ml"})[1].text
+    category = soup.findAll("a",{"class":"ml"})[1].text
     #카테고리
     book_info = soup.find("div",{"class":"p_goodstd03"}).find("table").text
     #책정보
@@ -67,25 +77,25 @@ for i in range(0,len(bookURL_List)):
     for i in range(0,len(book_info_parsing)):
         if unicode(book_info_parsing[i]).find(u"쪽")>=0:
             #-*- coding: utf-8 -*-
-            book_page = (book_info_parsing[i]).strip()
-            book_page = book_page[:len(book_page)-1]
+            page = (book_info_parsing[i]).strip()
+            page = page[:len(page)-1]
             #책 쪽수
         elif book_info_parsing[i].find("mm")>=0:
             if book_info_parsing[i].find("(")>=0:
-                book_size = book_info_parsing[i].split("(")[0].strip()
+                size = book_info_parsing[i].split("(")[0].strip()
                 #책 크기
             else:
-                book_size = book_info_parsing[i].strip()
+                size = book_info_parsing[i].strip()
                 #책 크기
         elif unicode(book_info_parsing[i]).find(u"본")>=0:
             #-*- coding: utf-8 -*-
-            book_cover = book_info_parsing[i].strip()
+            cover = book_info_parsing[i].strip()
             #책 커버
         elif book_info_parsing[i].find("g")>=0:
-            book_weight = book_info_parsing[i].replace("g","").strip()
+            weight = book_info_parsing[i].replace("g","").strip()
             #책 무게
         elif book_info_parsing[i].find("ISBN")>=0:
-            book_ISBN = book_info_parsing[i].split(":")[1].strip()
+            ISBN = book_info_parsing[i].split(":")[1].strip()
             #책 ISBN
         
     
@@ -98,57 +108,74 @@ for i in range(0,len(bookURL_List)):
         if unicode(book_writers).find(u"(지은이)")>=0:
             temp  = unicode(book_writers).split(u"(지은이)")
             #-*- coding: utf-8 -*-    
-            book_writer = temp[0].strip()
+            writer = temp[0].strip()
             
             if i>0:
                 for j in range(i-1,-1,-1):
-                    book_writer += ", " + book_info2_parsing[j].strip()
+                    writer += ", " + book_info2_parsing[j].strip()
         elif unicode(book_writers).find(u"(엮은이)")>=0:
             temp  = unicode(book_writers).split(u"(엮은이)")
             #-*- coding: utf-8 -*-
-            book_writer = temp[0].strip()
+            writer = temp[0].strip()
         elif unicode(book_writers).find(u"(글)")>=0:
             temp  = unicode(book_writers).split(u"(글)")
             #-*- coding: utf-8 -*-
-            book_writer = temp[0].strip()
+            writer = temp[0].strip()
         elif unicode(book_writers).find(u"(원작)")>=0:
             temp  = unicode(book_writers).split(u"(원작)")
             #-*- coding: utf-8 -*-
-            book_writer = temp[0].strip()
+            writer = temp[0].strip()
         
         #작가
-    if book_writer == "NA":
-        book_writer = book_info2_parsing[0]
+    if writer == "NA":
+        writer = book_info2_parsing[0]
 
     temp=0;
     if unicode(book_info2_parsing[len(book_info2_parsing)-1]).find(u"원제")>=0:
         temp=1;
 
     #-*- coding: utf-8 -*-
-    book_publisher = book_info2_parsing[len(book_info2_parsing)-2-temp].strip()
+    publisher = book_info2_parsing[len(book_info2_parsing)-2-temp].strip()
     #출판사
-    book_date = book_info2_parsing[len(book_info2_parsing)-1-temp].strip()
+    date = book_info2_parsing[len(book_info2_parsing)-1-temp].strip()
     #출판날짜
 
     #-*- coding: utf-8 -*-
 
-    print "name        : " , book_name
-    print "writer      : " , book_writer
-    print "date        : " , book_date
-    print "price       : " , book_price
-    print "cover       : " , book_cover
-    print "page        : " , book_page
-    print "size        : " , book_size
-    print "weight      : " , book_weight
-    print "category    : " , book_category
-    print "ISBN        : " , book_ISBN
-    print "publisher   : " , book_publisher
+    print "name        : " , name
+    print "writer      : " , writer
+    print "date        : " , date
+    print "price       : " , price
+    print "cover       : " , cover
+    print "page        : " , page
+    print "size        : " , size
+    print "weight      : " , weight
+    print "category    : " , category
+    print "ISBN        : " , ISBN
+    print "publisher   : " , publisher
     print "sales point : " , sales_point
     print "--------------------------------------------"
 
+    book_name.append(name)
+    book_writer.append(writer)
+    book_date.append(date)
+    book_price.append(price)
+    book_cover.append(cover)
+    book_page.append(page)
+    book_size.append(size)
+    book_weight.append(weight)
+    book_category.append(category)
+    book_ISBN.append(ISBN)
+    book_publisher.append(publisher)
+    book_sales_point.append(sales_point)
 
-    book_string = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (book_name, book_writer, book_publisher, book_date, book_price, book_page, book_size, book_cover, book_weight, book_category, book_ISBN, sales_point)
-    
-    fp.write(book_string.encode("utf-8"))
+    count = count+1
 
-fp.close()
+with open('./book_data.csv', 'w') as csvfile:
+    fieldnames = ['book_name','book_writer','book_date','book_price','book_cover','book_page','book_size','book_weight','book_category','book_ISBN','book_publisher','sales_point']
+    fwriter =  csv.DictWriter(csvfile, fieldnames=fieldnames)
+    fwriter.writeheader()
+    for i in range(0,count):
+        fwriter.writerow({'book_name':book_name.pop().encode('utf-8'),'book_writer':book_writer.pop().encode('utf-8'),'book_date':book_date.pop().encode('utf-8'),'book_price':book_price.pop().encode('utf-8'),'book_cover':book_cover.pop().encode('utf-8'),'book_page':book_page.pop().encode('utf-8'),'book_size':book_size.pop().encode('utf-8'),'book_weight':book_weight.pop().encode('utf-8'),'book_category':book_category.pop().encode('utf-8'),'book_ISBN':book_ISBN.pop().encode('utf-8'),'book_publisher':book_publisher.pop().encode('utf-8'),'sales_point':book_sales_point.pop().encode('utf-8')})
+
+
